3f90dddbc8321b7c68be881e0498e179ac9dcfad
M Sufyan Khan <msufyankhan709@gmail.com>
2021-09-16 11:33:15 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersDataService } from './users-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APIs';
+
+  constructor(private user:UsersDataService){
+    this.user.getData().subscribe(data=>{
+      console.warn(data);
+    })
+  }
 }
